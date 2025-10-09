e997c89b2ffbcc7c994f05deb2d6e786b89330b2
Sebastien DAMAYE <sebastien.damaye@se.com>
2025-10-09 20:27:14 +0200

    fixed incorrect upgrade script fr_61

--- qm/scripts/upgrade/fr_61.py
+++ qm/scripts/upgrade/fr_61.py
@@ -2,18 +2,19 @@
 FR #61 - Community repo for threat hunting analytics
 Migration script to populate the ConnectorDomain and Connector models
 """
-from repos.models import Connector
+from connectors.models import Connector
 
-connector = Connector.objects.get(name="microsoftsentinel")
-connector.domain = "analytics"
-connector.save()
+def run():
+    connector = Connector.objects.get(name="microsoftsentinel")
+    connector.domain = "analytics"
+    connector.save()
 
-connector = Connector.objects.get(name="sentinelone")
-connector.domain = "analytics"
-connector.save()
+    connector = Connector.objects.get(name="sentinelone")
+    connector.domain = "analytics"
+    connector.save()
 
-connector = Connector(name="github", description="Github repo sync", domain="repos")
-connector.save()
+    connector = Connector(name="github", description="Github repo sync", domain="repos")
+    connector.save()
 
-connector = Connector(name="bitbucket", description="Bitbucket repo sync", domain="repos")
-connector.save()
+    connector = Connector(name="bitbucket", description="Bitbucket repo sync", domain="repos")
+    connector.save()
